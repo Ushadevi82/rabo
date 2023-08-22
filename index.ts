a2fde1438146d8c3e8e64d0b40dabec5cba7f6ef
Ushadevi Devaraji <58028107+Ushadevi82@users.noreply.github.com>
2023-08-22 22:46:11 +0530

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/login', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server is running 1');
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server is running');
+});
+
+app.listen(port, () => {
+  console.log(` Server is running at http://localhost:${port}`);
+});
